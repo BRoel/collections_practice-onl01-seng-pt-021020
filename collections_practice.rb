a068cd6fe0de569d4bcd5662c2238a8e95eede0d
Brennan Roel <brennanroel@gmail.com>
2020-02-21 02:48:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
+def swap_elements(array)
+  array[1..2] = array[1..2].sort do |a,b|
+    if a==b || a<b || a>b 
+      1
+    end
+  end
+  array
+end
 
+test_array = [1,2,3,4]
+string_test = ["blake", "ashley", "scott"]
+
+def swap_elements_from_to(array, index, new_index)
+  array[index], array[new_index] = array[new_index], array[index]
+  return array
+end
