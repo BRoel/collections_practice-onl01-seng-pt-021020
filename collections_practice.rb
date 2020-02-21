420e9717418622bdd7afddc0bac8239f9dda7cea
Brennan Roel <brennanroel@gmail.com>
2020-02-21 03:05:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,82 +1,54 @@
-def swap_elements(array)
-  array[1..2] = array[1..2].sort do |a,b|
-    if a==b || a<b || a>b 
-      1
-    end
-  end
-  array
+def sort_array_asc(integers)
+  integers.sort
 end
 
-test_array = [1,2,3,4]
-string_test = ["blake", "ashley", "scott"]
-#swap_elements(test_array)
+def sort_array_desc(integers)
+  integers.sort {|first_num, second_num| second_num <=> first_num}
+end
 
-def swap_elements_from_to(array, index, new_index)
-  array[index], array[new_index] = array[new_index], array[index]
+def sort_array_char_count(strings)
+  strings.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(strings)
+  strings[1], strings[2] = strings[2], strings[1]
+  return strings
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
   return array
 end
 
-describe 'swap_elements_from_to' do
-  it 'swaps elements and allows you to specify the index of the element you would like to move to a new index' do
-    expect(swap_elements_from_to(["one", "two", "three"], 2, 1)).to eq(["one", "three", "two"])
-  end
+def reverse_array(integers)
+  new_array = integers.reverse
+  new_array
 end
 
-
 def kesha_maker(array)
-  return_array = []
-  array.each do |string|
-    string[3] = "$"
-    return_array << string
+  kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
   end
-  return_array
+  kesha
 end
 
 def find_a(array)
-  array.select do |string|
-    string.start_with?("a")
-  end
+  array.select{|string| string.start_with?("a")}
 end
 
-
-def sum_array(array)
-     
-  sum = 0
-  array. each do |number|
-    if number.is_a? Integer
-      sum += number
-    end
-  end
-  sum
+def sum_array(integers)
+  integers.inject{|sum, n| sum + n}
 end
-
-def sum_array_inject(array)
-    array.inject do |sum,number| 
-        sum + number
-    end
-end
-
-number_array =[0,1.07,2,3,4,5.2,6]
 
 def add_s(array)
-  
-  return_array = []
-      array.each_with_index do |word,index|
-        if index != 1 
-          word = word + "s"
-        end
-        return_array << word 
-  end
-  
-  return_array
-end
-
-def add_s_collect(array)
-      array.each_with_index.collect do |word,index|
-        if index != 1 
-          word = word + "s"
-        else
-          word = word
-        end
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
+    else
+      string << "s"
+    end
   end
 end
