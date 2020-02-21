a5029be316f2e2fa4a37d52f872ec92d9a91b0fc
Brennan Roel <brennanroel@gmail.com>
2020-02-21 02:59:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,8 +1,8 @@
-def sort_array(array)
+def sort_array_asc(array)
   array.sort
 end
 
-sort_array([25, 7, 1]) #[1,7,25]
+sort_array_asc([25, 7, 1]) #[1,7,25]
 
 def sort_array_desc(array)
   array.sort do | left, right|
@@ -12,13 +12,13 @@ end
 
 sort_array_desc([25, 7, 14]) #[25, 14, 7]
 
-def sort_array_count(array)
+def sort_array_char_count(array)
   array.sort do |left, right|
     left.length <=> right.length
   end
 end
 
-sort_array_count(["dogs", "cat", "Horses"]) 
+sort_array_char_count(["dogs", "cat", "Horses"]) 
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
@@ -31,28 +31,23 @@ def reverse_array(array)
   array.reverse
 end
 
-reverse_array(["blake", "ashley", "scott"]) #["scott", "ashley", "blake"]
+reverse_array(["blake", "ashley", "scott"]) 
 
-def names(array)
+def kesha_maker(array)
   array.each do |item|
     item[2] = "$"
   end
 end
-
-names["blake", "ashley", "scott"]) #["bl$ke", "as$ley", "sc$tt"]
 
 def find_a(array)
   array.find_all do |word|
     word[0] == "a"
   end
 
-  # using select method
-    # array.select do |word|
-    #   word[0] == "a"
-    # end
+ 
 end
 
-find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ])
+find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) 
 
 def sum_array(array)
   sum = 0
