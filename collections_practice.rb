bbf4dbe958f4fe20dae4b22f654d002fa038a049
Brennan Roel <brennanroel@gmail.com>
2020-02-21 02:52:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,3 +15,38 @@ def swap_elements_from_to(array, index, new_index)
   return array
 end
 
+a_maker(["blake", "ashley", "scott"]) #["bl$ke", "as$ley", "sc$tt"]
+
+def find_a(array)
+  array.find_all do |word|
+    word[0] == "a"
+  end
+
+ 
+end
+
+find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) #["apple", "avis", "arlo", "ascot"]
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum+=num
+  end
+  sum
+end 
+ 
+end
+
+sum_array([11,4,7,8,9,100,134]) #273
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
+
+add_s(["hand","feet", "knee", "table"]) #["hands","feet", "knees", "tables"]
