cc23753b0a51cd41e482294dbd4f0ecf12e55dc7
Brennan Roel <brennanroel@gmail.com>
2020-02-21 02:59:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,20 +18,20 @@ def sort_array_char_count(array)
   end
 end
 
-sort_array_char_count(["dogs", "cat", "Horses"]) 
+sort_array_char_count(["dogs", "cat", "Horses"]) #["cat", "dogs", "Horses"]
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
 
-swap_elements(["blake", "ashley", "scott"]) 
+swap_elements(["blake", "ashley", "scott"]) #["blake", "scott", "ashley"]
 
 def reverse_array(array)
   array.reverse
 end
 
-reverse_array(["blake", "ashley", "scott"]) 
+reverse_array(["blake", "ashley", "scott"]) #["scott", "ashley", "blake"]
 
 def kesha_maker(array)
   array.each do |item|
@@ -39,15 +39,20 @@ def kesha_maker(array)
   end
 end
 
+kesha_maker(["blake", "ashley", "scott"]) #["bl$ke", "as$ley", "sc$tt"]
+
 def find_a(array)
   array.find_all do |word|
     word[0] == "a"
   end
 
- 
+  # using select method
+    # array.select do |word|
+    #   word[0] == "a"
+    # end
 end
 
-find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) 
+find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) #["apple", "avis", "arlo", "ascot"]
 
 def sum_array(array)
   sum = 0
@@ -56,7 +61,16 @@ def sum_array(array)
   end
   sum
 end 
-  
+  # using reduce method
+    # array.reduce(:+)
+
+  # using inject method (short)
+     # array.inject(:+)
+
+  # using inject method (long)
+     # array.inject do |sum,x|
+     #  sum + x
+     # end
 end
 
 sum_array([11,4,7,8,9,100,134]) #273
